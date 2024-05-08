b152a7c50d2f2d3e76dae350f96db8e31605bfc5
harvey desktop <harveygit@pm.me>
2024-05-08 16:06:09 -0500

    main window class created

--- src/ui/mainwindow.hpp
+++ src/ui/mainwindow.hpp
@@ -9,7 +9,56 @@
 #include <QMenuBar>
 #include <QMenu>
 #include <QStatusBar>
+#include <string>
 
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
 
+    public:
+        MainWindow(const QString &title, QWidget *parent);
+
+    protected:
+        void closeEvent(QCloseEvent *event) override;
+
+    private slots:
+        void print();
+        void open();
+        void save(std::string="");// Handles save and save as
+        void cut();
+        void copy();
+        void paste();
+        void selectAll();
+        void del();// Shortened name for delete prevents conflict
+        void undo();
+        void redo();
+    
+    private:
+        void createActions();
+        void createMenus();
+        bool confirmUnsaved();
+
+    private:
+        QTextEdit *textEditor;
+        QScrollBar *scrollBar;
+        QStatusBar *statusBar;
+
+        QMenu *menuFile;
+        QMenu *menuEdit;
+
+        QAction actionNew;
+        QAction actionOpen;
+        QAction actionSave;
+        QAction actionSaveAs;
+        QAction actionPrint;
+        QAction actionCut;
+        QAction actionCopy;
+        QAction actionPaste;
+        QAction actionUndo;
+        QAction actionRedo;
+        QAction actionDelete;
+        QAction actionSelectAll;
+
+};
 
 #endif // MAINWINDOW_H_
