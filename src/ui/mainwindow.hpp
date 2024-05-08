2fc6520bc359a67d206aa85598afacbe0a1512e1
harvey desktop <harveygit@pm.me>
2024-05-08 14:14:06 -0500

    Includes for used Q classes

--- src/ui/mainwindow.hpp
+++ src/ui/mainwindow.hpp
@@ -1,6 +1,15 @@
 #ifndef MAINWINDOW_H_
 #define MAINWINDOW_H_
 
+#include <QAction>
+#include <QMainWindow>
+#include <QWidget>
+#include <QTextEdit>
+#include <QScrollBar>
+#include <QMenuBar>
+#include <QMenu>
+#include <QStatusBar>
+
 
 
 #endif // MAINWINDOW_H_
