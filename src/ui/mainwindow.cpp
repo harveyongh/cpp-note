c8684f709b1a264662adb04ede94c24d4c3b5ca1
harvey desktop <harveygit@pm.me>
2024-05-08 16:30:00 -0500

    Start on implementation

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,1 +1,9 @@
 #include "mainwindow.hpp"
+#include "../editor/texteditor.hpp"
+
+using namespace cppnote;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), textEditor(new TextEditor(this))
+{
+}
