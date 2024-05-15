94cb7ac40555150c0add8c93568a153c3dec1d2a
harvey desktop <harveygit@pm.me>
2024-05-14 21:55:41 -0500

    reformat mainwindow header

--- src/ui/mainwindow.hpp
+++ src/ui/mainwindow.hpp
@@ -11,57 +11,50 @@
 #include <QStatusBar>
 #include <QString>
 #include <string>
+#include "../editor/texteditor.hpp"
 
-namespace cppnote{
-    class MainWindow : public QMainWindow
-    {
-        Q_OBJECT
-
-        public:
-            MainWindow(QWidget *parent);
-
-        protected:
-            void closeEvent(QCloseEvent *event) override;
-
-        private slots:
-            void print();
-            void open();
-            void save(std::string="");// Handles save and save as
-            void cut();
-            void copy();
-            void paste();
-            void selectAll();
-            void del();// Shortened name for delete prevents conflict
-            void undo();
-            void redo();
-
-        private:
-            void createActions();
-            void createMenus();
-            bool confirmUnsaved();
-
-        private:
-            QTextEdit *textEditor;
-            QScrollBar *scrollBar;
-            QStatusBar *statusBar;
-
-            QMenu *menuFile;
-            QMenu *menuEdit;
-
-            QAction actionNew;
-            QAction actionOpen;
-            QAction actionSave;
-            QAction actionSaveAs;
-            QAction actionPrint;
-            QAction actionCut;
-            QAction actionCopy;
-            QAction actionPaste;
-            QAction actionUndo;
-            QAction actionRedo;
-            QAction actionDelete;
-            QAction actionSelectAll;
-
-    };
-}
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    public:
+        MainWindow(QWidget *parent);
+    protected:
+        void closeEvent(QCloseEvent *event) override;
+    private slots:
+        void print();
+        void open();
+        void save(std::string="");// Handles save and save as
+    signals:
+        // Signals sent to editor
+        void cut();
+        void copy();
+        void paste();
+        void selectAll();
+        void del();// Shortened name for delete prevents conflict
+        void undo();
+        void redo();
+    private:
+        void createActions();
+        void createMenus();
+        bool confirmUnsaved();
+    private:
+        QTextEdit *textEditor;
+        QScrollBar *scrollBar;
+        QStatusBar *statusBar;
+        QMenu *menuFile;
+        QMenu *menuEdit;
+        QAction actionNew;
+        QAction actionOpen;
+        QAction actionSave;
+        QAction actionSaveAs;
+        QAction actionPrint;
+        QAction actionCut;
+        QAction actionCopy;
+        QAction actionPaste;
+        QAction actionUndo;
+        QAction actionRedo;
+        QAction actionDelete;
+        QAction actionSelectAll;
+};
 
 #endif // MAINWINDOW_H_
