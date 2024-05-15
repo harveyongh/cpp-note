cd5fb26342c46e914fc59ee53ec1d122332ea5de
harvey desktop <harveygit@pm.me>
2024-05-15 10:51:39 -0500

    layout settings added

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,7 +1,27 @@
 #include "mainwindow.hpp"
-#include "../editor/texteditor.hpp"
 
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent) {
+CnoteWindow::CnoteWindow(QWidget *parent) :
+    QWidget(parent) {
+        menuBar = new QMenuBar(this);
+        createMenus();
+        //scrollBar = new QScrollBar(this);
+        textEditor = new QTextEdit(this);
+        // Layout settings
+        setWindowTitle("&C++Note");
+        resize(600, 400);
+        //windowLayout = new QGridLayout(this);
+        //windowLayout->addWidget(menuBar, 0, 0, 2, 3);
+        //windowLayout->addWidget(textEditor, 1, 0, 2, 3);
+        //windowLayout->addWidget(scrollBar, 1, 3);
+        vbox = new QVBoxLayout(this);
+        vbox->addWidget(menuBar);
+        vbox->addWidget(textEditor);
+}
+
+void CnoteWindow::createMenus(){
+    
+    menuFile = menuBar->addMenu("&File");
+    menuEdit = menuBar->addMenu("&Edit");
+
 }
