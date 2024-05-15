962a201ababa0a2a3294f794bc1a6b90b269affd
harvey desktop <harveygit@pm.me>
2024-05-15 10:50:54 -0500

    prevent conflicts with cnote class name

--- src/cppnote.cpp
+++ src/cppnote.cpp
@@ -1,15 +1,15 @@
 /*
 This is the main project file for the CPPNote project
 */
-
+#include "ui/mainwindow.hpp"
 #include <QApplication>
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
-    //MainWindow window;
-    //window.show();
+    CnoteWindow window;
+    window.show();
 
     return app.exec();
 }
