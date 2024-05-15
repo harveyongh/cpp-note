066fcabc78f1b9793e10ece3b849bd5f4f6ed56c
harvey desktop <harveygit@pm.me>
2024-05-14 21:51:16 -0500

    wrote start of editor header, commented slots

--- src/editor/texteditor.hpp
+++ src/editor/texteditor.hpp
@@ -1,9 +1,21 @@
 #include <QTextEdit>
 
-namespace cppnote{
-    class TextEditor : public QTextEdit
-    {
-        //
-    };
 
-}
+class CnoteTextEditor : public QTextEdit
+{
+    Q_OBJECT
+    public:
+        CnoteTextEditor(QTextEdit *parent);
+    
+    private slots:
+        //QTextEdit is capable of handling these behaviors...
+        //To test if these slots are needed for anything
+        //void cut();
+        //void copy();
+        //void paste();
+        //void selectAll();
+        //void del();// Shortened name for delete prevents conflict
+        //void undo();
+        //void redo();
+};
+
