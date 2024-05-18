fe35ab7b48dda52a37ffb8e6441423cfd177740c
harvey desktop <harveygit@pm.me>
2024-05-18 14:05:27 -0500

    class declaration for buttonbar created

---
+++ src/ui/buttonbar.hpp
@@ -0,0 +1,28 @@
+#ifndef BUTTONBAR_HPP
+#define BUTTONBAR_HPP
+
+#include <QToolButton>
+#include <QToolBar>
+
+class CnoteButtonBar : public QWidget
+{
+    Q_OBJECT
+    public:
+        CnoteButtonBar(QWidget *parent);
+
+    private:
+        QToolBar *buttonBar;
+
+        QToolButton *buttonNew;
+        QToolButton *buttonOpen;
+        QToolButton *buttonSave;
+
+        QToolButton *buttonCut;
+        QToolButton *buttonCopy;
+        QToolButton *buttonPaste;
+        QToolButton *buttonUndo;
+        QToolButton *buttonRedo;
+
+};
+
+#endif //BUTTONBAR_HPP
